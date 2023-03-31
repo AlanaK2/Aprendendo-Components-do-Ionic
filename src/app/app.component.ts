97c1db1e78b3c8c8910588dcb9ceb32ca05e5975
Alana <alanakarine27@gmail.com>
2023-03-31 16:08:13 -0300

    component checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
     { title: 'Action-Sheet', url: 'actionsheet', icon: 'construct' },
     { title: 'Badge', url: 'badge', icon: 'construct' },
     { title: 'Card', url: 'card', icon: 'construct' },
+    { title: 'Checkbox', url: 'checkbox', icon: 'construct' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
