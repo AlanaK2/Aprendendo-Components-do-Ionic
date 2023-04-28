2873d4d95d154a6d1de3360871702d454a0cf4c0
Alana <alanakarine27@gmail.com>
2023-04-28 09:51:19 -0300

    content e datetime component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,8 @@ export class AppComponent {
     { title: 'Card', url: 'card', icon: 'construct' },
     { title: 'Checkbox', url: 'checkbox', icon: 'construct' },
     { title: 'Chip', url: 'chip', icon: 'construct' },
+    { title: 'Content', url: 'content', icon: 'construct' },
+    { title: 'DateTime', url: 'datetime', icon: 'construct' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
